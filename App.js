9c0735b4056323f2483ff20eab1dccef27068357
anandzhang <hi@anandzhang.com>
2021-09-14 15:41:45 +0800

    使用 react-native 脚手架创建基础项目

---
+++ App.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from "react-native"
+
+const App = () => (
+  <SafeAreaView>
+    <View>
+      <Text>React Native Web Example</Text>
+    </View>
+  </SafeAreaView>
+)
+
+export default App
